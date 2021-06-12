910e28a464811b5db9a6b5a409d2fe023ee248a7
Ученик <sinokbati@gmail.com>
2021-06-12 16:54:27 +0300

    НАШЕ первое сохранение

--- optimizacia1.4.py
+++ optimizacia1.4.py
@@ -69,6 +69,7 @@ class optimization_cutting:
             print('|' * (int(sum(i))), '.' * int((200 - (sum(i)))))
         return self.luchiy_raskroy
     print('Учим гит')
+    print('Первый комит')
 if __name__ == '__main__':
     proba_1 = optimization_cutting((23, 23, 23, 58, 61, 78, 81, 86, 86, 91, 94, 97, 105, 112), 200, 0)
     print(proba_1.get_raskroy())
